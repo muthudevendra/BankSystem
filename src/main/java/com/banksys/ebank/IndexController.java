58e671d72a05bff148469d5c68c883b69a52e582
Lakshitha <ranasinghe5@gmail.com>
2017-06-08 17:19:25 +0530

    Layout Changes

--- src/main/java/com/banksys/ebank/IndexController.java
+++ src/main/java/com/banksys/ebank/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/ebank")
-public class EBankIndexController {
+public class IndexController {
     @RequestMapping("/")
     public String test(){
         return "ebank";
