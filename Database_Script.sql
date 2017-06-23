b9cc06e5423033336beead1a69293e50f1d88ede
lakshithar <lakshithar@itx360.com>
2017-06-23 12:37:00 +0530

    New DB Script

--- Database_Script.sql
+++ Database_Script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2017 at 03:05 PM
+-- Generation Time: Jun 23, 2017 at 07:05 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -42,7 +42,10 @@ CREATE TABLE `authority` (
 --
 
 INSERT INTO `authority` (`AUTHORITY_ID`, `AUTHORITY_NAME`, `MODULE_ID`, `STATUS`, `CREATED_BY`, `CREATED_DATE`, `LAST_MODIFIED_BY`, `LAST_MODIFIED_DATE`) VALUES
-(1, 'EBank@EBank_VIEW', 1, 1, 'SYSTEM', '2017-06-20', 'SYSTEM', '2017-06-20');
+(1, 'ebank@ebank_VIEW', 1, 1, 'SYSTEM', '2017-06-20', 'SYSTEM', '2017-06-20'),
+(2, 'admin@admin_VIEW', 2, 1, 'SYSTEM', '2017-06-21', 'SYSTEM', '2017-06-21'),
+(3, 'module@moduleManagement_VIEW', 2, 1, 'SYSTEM', '2017-06-21', 'SYSTEM', '2017-06-21'),
+(4, 'module@moduleManagement_CREATE', 2, 1, 'SYSTEM', '2017-06-23', 'SYSTEM', '2017-06-23');
 
 -- --------------------------------------------------------
 
@@ -66,7 +69,8 @@ CREATE TABLE `module` (
 --
 
 INSERT INTO `module` (`MODULE_ID`, `MODULE_NAME`, `DESCRIPTION`, `STATUS`, `CREATED_BY`, `CREATED_DATE`, `LAST_MODIFIED_BY`, `LAST_MODIFIED_DATE`) VALUES
-(1, 'EBANK', 'EBank Home', 1, 'SYSTEM', '2017-06-20', '1', '2017-06-20');
+(1, 'EBANK', 'EBank Home', 1, 'SYSTEM', '2017-06-20', '1', '2017-06-20'),
+(2, 'Admin', 'Admin Module', 1, 'SYSTEM', '2017-06-23', 'SYSTEM', '2017-06-23');
 
 -- --------------------------------------------------------
 
@@ -92,6 +96,34 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`USER_ID`, `USERNAME`, `PASSWORD`, `ENABLED`, `USER_TYPE_ID`, `CREATED_BY`, `CREATED_DATE`, `LAST_MODIFIED_BY`, `LAST_MODIFIED_DATE`) VALUES
 (2, 'lakiya', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, 1, 'SYSTEM', '2017-06-20', 'SYSTEM', '2017-06-20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login_audit`
+--
+
+CREATE TABLE `user_login_audit` (
+  `USER_LOGIN_AUDIT_ID` int(11) NOT NULL,
+  `USERNAME` varchar(256) NOT NULL,
+  `LOGIN_DATE` date NOT NULL,
+  `LOGOUT_DATE` date DEFAULT NULL,
+  `REMOTE_ADDRESS` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_login_audit`
+--
+
+INSERT INTO `user_login_audit` (`USER_LOGIN_AUDIT_ID`, `USERNAME`, `LOGIN_DATE`, `LOGOUT_DATE`, `REMOTE_ADDRESS`) VALUES
+(1, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1'),
+(2, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1'),
+(3, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1'),
+(4, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1'),
+(5, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1'),
+(6, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1'),
+(7, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1'),
+(8, 'lakiya', '2017-06-23', NULL, '0:0:0:0:0:0:0:1');
 
 -- --------------------------------------------------------
 
@@ -139,7 +171,10 @@ CREATE TABLE `user_type_authority` (
 --
 
 INSERT INTO `user_type_authority` (`USER_TYPE_AUTHORITY_ID`, `USER_TYPE_ID`, `AUTHORITY_ID`, `STATUS`, `CREATED_BY`, `CREATED_DATE`, `LAST_MODIFIED_BY`, `LAST_MODIFIED_DATE`) VALUES
-(1, 1, 1, 1, 'SYSTEM', '2017-06-20', 'SYSTEM', '2017-06-20');
+(1, 1, 1, 1, 'SYSTEM', '2017-06-20', 'SYSTEM', '2017-06-20'),
+(2, 1, 2, 1, 'SYSTEM', '2017-06-21', 'SYSTEM', '2017-06-21'),
+(3, 1, 3, 1, 'SYSTEM', '2017-06-21', 'SYSTEM', '2017-06-21'),
+(4, 1, 4, 1, 'SYSTEM', '2017-06-23', 'SYSTEM', '2017-06-23');
 
 --
 -- Indexes for dumped tables
@@ -164,6 +199,12 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`USER_ID`);
 
 --
+-- Indexes for table `user_login_audit`
+--
+ALTER TABLE `user_login_audit`
+  ADD PRIMARY KEY (`USER_LOGIN_AUDIT_ID`);
+
+--
 -- Indexes for table `user_type`
 --
 ALTER TABLE `user_type`
@@ -183,17 +224,22 @@ ALTER TABLE `user_type_authority`
 -- AUTO_INCREMENT for table `authority`
 --
 ALTER TABLE `authority`
-  MODIFY `AUTHORITY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `AUTHORITY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `module`
 --
 ALTER TABLE `module`
-  MODIFY `MODULE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `MODULE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user_login_audit`
+--
+ALTER TABLE `user_login_audit`
+  MODIFY `USER_LOGIN_AUDIT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `user_type`
 --
@@ -203,7 +249,7 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT for table `user_type_authority`
 --
 ALTER TABLE `user_type_authority`
-  MODIFY `USER_TYPE_AUTHORITY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `USER_TYPE_AUTHORITY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
