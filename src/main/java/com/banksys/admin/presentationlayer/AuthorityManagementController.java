937b28c08f4994c638954781706742dedae7ec71
Lakshitha <ranasinghe5@gmail.com>
2017-06-25 21:35:47 +0530

    Minor Changes in Oshi's code

--- src/main/java/com/banksys/admin/presentationlayer/AuthorityManagementController.java
+++ src/main/java/com/banksys/admin/presentationlayer/AuthorityManagementController.java
@@ -2,7 +2,7 @@ package com.banksys.admin.presentationlayer;
 
 import com.banksys.admin.datalayer.entity.Authority;
 import com.banksys.admin.datalayer.service.ModuleService;
-import com.banksys.util.enums.Gender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Oshada on 6/25/2017.
+ *
  */
 
 @Controller
@@ -18,25 +19,16 @@ public class AuthorityManagementController {
 
     private final ModuleService moduleService;
 
+    @Autowired
     public AuthorityManagementController(ModuleService moduleService) {
         this.moduleService = moduleService;
     }
 
     @GetMapping
-    public String getPage(Model model)
-    {
-        model = this.getPageData(model);
+    public String getPage(Model model) {
         model.addAttribute("authority", new Authority());
         System.out.println("size"+moduleService.count());
         return "authorityManagement";
 
     }
-
-
-    private Model getPageData(Model model){
-        model.addAttribute("genderList", Gender.values());
-        return model;
-    }
-
-
 }
