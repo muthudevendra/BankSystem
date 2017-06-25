a33000d652e0b77c6c7e9f57477adc1d72d02b42
Mafrin <oviraj71@gmail.com>
2017-06-25 21:46:31 +0530

    Authority modified

--- src/main/java/com/banksys/admin/presentationlayer/AuthorityManagementController.java
+++ src/main/java/com/banksys/admin/presentationlayer/AuthorityManagementController.java
@@ -1,12 +1,15 @@
 package com.banksys.admin.presentationlayer;
 
+import com.banksys.admin.businesslayer.managerImpl.AuthorityManagementControllerManagermpl;
 import com.banksys.admin.datalayer.entity.Authority;
 import com.banksys.admin.datalayer.service.ModuleService;
 import com.banksys.util.enums.Gender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Oshada on 6/25/2017.
@@ -38,7 +41,7 @@ public class AuthorityManagementController {
     }
 
     @RequestMapping(value = "/saveAuthority" ,method = RequestMethod.POST)
-    public String saveAuthority(@ModelAttribute Authority authority ,Model model) {
+    public String saveAuthority(@ModelAttribute Authority authority , Model model) {
 //        this.authorityManagementControllerManager.saveAuthority(authority);
 //        model.addAttribute("authority",authority);
         System.out.println(">>>>>>>>>>>>>>>>>>>>>");
