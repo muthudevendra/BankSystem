421dd731da45a87ef51c13cd9c2e59a698fbabe0
Muthu Devendra <muthupriyadarshani@gmail.com>
2017-06-28 23:11:24 +0530

    Additional

--- src/main/java/com/banksys/admin/presentationlayer/UserTypeManagementController.java
+++ src/main/java/com/banksys/admin/presentationlayer/UserTypeManagementController.java
@@ -1,8 +1,17 @@
 package com.banksys.admin.presentationlayer;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Oshada on 6/27/2017.
  */
+@Controller
+@RequestMapping("/admin/config/userTypeManagement")
 public class UserTypeManagementController {
-
+    @GetMapping
+    public String getPage() {
+        return "userTypeManagement";
+    }
 }
