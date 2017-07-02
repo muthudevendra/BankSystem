3453215eae7e4cf34751518dcd33cc5d9a159e30
Lakshitha <ranasinghe5@gmail.com>
2017-07-02 22:39:41 +0530

    Minor Fix

--- src/main/webapp/admin/script/userTypeSearch.js
+++ src/main/webapp/admin/script/userTypeSearch.js
@@ -1,12 +1,13 @@
 /**
  * Created by Lakshitha on 02-Jul-17.
+ *
  */
 
 $(function () {
     $('.navUrl').removeClass('active');
     $('#userTypeSearch').addClass('active');
     $('.datatable').dataTable({
-        order: [[ 3, "desc" ]],
+        order: [[ 1, "desc" ]],
         dom: "B<'row'<'pull-left'l><'pull-right'f>><'row'<'col-sm-12'tr>><br><'row'<'pull-left'i><'pull-right'p>>",
         language: { search: "_INPUT_", searchPlaceholder: "Search"}
     });
